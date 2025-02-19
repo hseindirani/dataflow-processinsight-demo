d41c1e0bf44f60ae7289a1eea6a7eea9b3dcf1bf
hseindirani <dirani.hussein93@gmail.com>
2025-02-19 12:06:27 +0100

    Add Spring Boot project setup

---
+++ src/main/java/com/dataflowprocessinsightdemo/DataflowprocessinsightdemoApplication.java
@@ -0,0 +1,13 @@
+package com.dataflowprocessinsightdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataflowprocessinsightdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataflowprocessinsightdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataflowprocessinsightdemo/DataflowprocessinsightdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataflowprocessinsightdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataflowprocessinsightdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
